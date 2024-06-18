e1aa0695c5ced62f7173de12477e819c9c3d54d1
rezmes <rezmes@gmail.com>
2024-06-18 12:09:47 +0330

    copied public folder

---
+++ src/components/Great.ts
@@ -0,0 +1,7 @@
+import React from "react";
+function Greet(params:type) {
+
+    return <h1>hello {params.name}</h1>
+    
+}
+export default Greet
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import * as React from 'react';
+import logo from './logo.svg';
 import * as ReactDOM from 'react-dom';
 
-const App = () => <div>Hello, React 15بله چنین است  with TypeScript!</div>;
+//const App = () => <div>Hello, React 15بله چنین است  with TypeScript!</div>;
+class App extends React.Component {
+  render(){
+    return(
+
+      <div className='App'>
+        <header className='App-header'>
+          <img src="{logo}" alt="logo" className='App-logo' />
+          <p>
+            Hello World!
+          </p>
+        </header>
+      </div>
+
+    )
+  }
+ // constructor(parameters) {
+    
+ // }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
